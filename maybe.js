e8c671fa59a07c5d22f9572c983ba11e74d4d9d5
Raynos <raynos2@gmail.com>
2013-01-17 17:38:20 -0800

    make maybe lazy

--- maybe.js
+++ maybe.js
@@ -3,13 +3,17 @@ var cleanse = require("./cleanse")
 module.exports = Maybe
 
 function Maybe(validator) {
-    validator = cleanse(validator)
+    var cleansed
 
     return function maybe(value, key) {
+        if (!cleansed) {
+            cleansed = cleanse(validator)
+        }
+
         if (value === null || value === undefined) {
             return
         }
 
-        return validator(value, key)
+        return cleansed(value, key)
     }
 }
